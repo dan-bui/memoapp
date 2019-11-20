658a58d65804e423d2edbbd17c54dbd95d0f6d6a
dan-bui <57938560+dan-bui@users.noreply.github.com>
2019-11-20 15:09:36 +0900

    Create MySchedulerApplication.java

---
+++ MySchedulerApplication.java
@@ -0,0 +1,22 @@
+package com.example.username.colen;
+
+import android.app.Application;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+public class MySchedulerApplication extends Application {
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        Realm.init(this);
+        RealmConfiguration realmConfig = new RealmConfiguration.Builder().build();
+        Realm.setDefaultConfiguration(realmConfig);
+
+
+    }
+}
