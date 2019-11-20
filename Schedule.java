f92a5b406a8d188a47686b9d976d818f2940b67a
dan-bui <57938560+dan-bui@users.noreply.github.com>
2019-11-20 15:10:31 +0900

    Create Schedule.java

---
+++ Schedule.java
@@ -0,0 +1,46 @@
+package com.example.username.colen;
+
+import java.util.Date;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class Schedule extends RealmObject {
+    @PrimaryKey
+    private long id;
+    private Date date;
+    private String title;
+    private String detail;
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
